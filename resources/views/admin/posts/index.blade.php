ce08e3c033d3f580019415ff1e9ad25b45d473b7
filippotabbi <filippotabbi23@gmail.com>
2021-05-26 17:50:10 +0200

    first commit

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+@foreach ($posts as $post)
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+        </div>
+    </div>
+</div>
+ @endforeach
+
+@endsection
